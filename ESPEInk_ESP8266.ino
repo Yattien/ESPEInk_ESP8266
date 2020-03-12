ca39e5ea19c24fa2689c2b022d82b6972fd55105
Yattien <36767821+Yattien@users.noreply.github.com>
2020-03-12 16:30:00 +0100

    refactored context class

--- ESPEInk_ESP8266.ino
+++ ESPEInk_ESP8266.ino
@@ -21,6 +21,7 @@
 #include <ArduinoJson.h>
 #include <FS.h>
 #include <PubSubClient.h>
+#include "ctx.h"
 
 #include "scripts.h"    // JavaScript code
 #include "css.h"        // Cascading Style Sheets
@@ -45,75 +46,6 @@ bool isUpdateAvailable = false;
 bool isDisplayUpdateRunning = false;
 bool isMqttEnabled = false;
 
-class Ctx {
-public:
-	Ctx() :
-			mqttPort(1883),
-					sleepTime(60) {
-		strcpy(mqttUpdateStatusTopic, "stat/display/needUpdate");
-		strcpy(mqttCommandTopic, "cmd/display/upload");
-	}
-	~Ctx() {
-		if (customMqttServer) {
-			delete customMqttServer;
-		}
-		if (customMqttPort) {
-			delete customMqttPort;
-		}
-		if (customMqttUpdateStatusTopic) {
-			delete customMqttUpdateStatusTopic;
-		}
-		if (customMqttCommandTopic) {
-			delete customMqttCommandTopic;
-		}
-		if (customSleepTime) {
-			delete customSleepTime;
-		}
-		if (customFirmwareUrl) {
-			delete customFirmwareUrl;
-		}
-	}
-	void initWifiManagerParameters() {
-		customMqttServer = new WiFiManagerParameter("server", "MQTT server", mqttServer, 40);
-		itoa(mqttPort, mqttPortAsString, 10);
-		customMqttPort = new WiFiManagerParameter("port", "MQTT port", mqttPortAsString, 6);
-		customMqttUpdateStatusTopic = new WiFiManagerParameter("updateTopic", "MQTT update topic", mqttUpdateStatusTopic, 128);
-		customMqttCommandTopic = new WiFiManagerParameter("commandTopic", "MQTT command topic", mqttCommandTopic, 128);
-		itoa(sleepTime, sleepTimeAsString, 10);
-		customSleepTime = new WiFiManagerParameter("sleepTime", "sleep time in seconds", sleepTimeAsString, 33);
-		customFirmwareUrl = new WiFiManagerParameter("firmwareUrl", "base URL for firmware images", firmwareUrl, 128);
-	}
-	void updateParameters() {
-		strcpy(mqttServer, customMqttServer->getValue());
-		mqttPort = atoi(customMqttPort->getValue());
-		strcpy(mqttUpdateStatusTopic, customMqttUpdateStatusTopic->getValue());
-		strcpy(mqttCommandTopic, customMqttCommandTopic->getValue());
-		sleepTime = atoi(customSleepTime->getValue());
-		strcpy(firmwareUrl, customFirmwareUrl->getValue());
-
-		if (strlen(mqttServer) > 0) {
-			isMqttEnabled = true;
-		}
-	}
-
-	char mqttServer[40];
-	char mqttPortAsString[6];
-	int mqttPort;
-	char mqttUpdateStatusTopic[128];
-	char mqttCommandTopic[128];
-	char sleepTimeAsString[33];
-	long sleepTime;
-	char firmwareUrl[128];
-
-	WiFiManagerParameter *customMqttServer;
-	WiFiManagerParameter *customMqttPort;
-	WiFiManagerParameter *customMqttUpdateStatusTopic;
-	WiFiManagerParameter *customMqttCommandTopic;
-	WiFiManagerParameter *customSleepTime;
-	WiFiManagerParameter *customFirmwareUrl;
-
-};
-
 Ctx ctx;
 
 // -----------------------------------------------------------------------------------------------------
@@ -127,6 +59,8 @@ void setup() {
 	ctx.initWifiManagerParameters();
 	setupWifi(ctx);
 	ctx.updateParameters();
+	isMqttEnabled = ctx.isMqttEnabled();
+
 
 	Serial.printf(" MQTT Server: %s:%d\n", ctx.mqttServer, ctx.mqttPort);
 	Serial.printf(" MQTT UpdateStatusTopic: %s\n", ctx.mqttUpdateStatusTopic);
